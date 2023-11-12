b3f5c21673cbbde03aa7ed838a2c2f5e8c3ed0b5
Tyler O'Neil <tyleroneil72@gmail.com>
2023-11-12 14:48:48 -0500

    Add comment to vite config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
+  // Load env variables from .env files in root directory
   process.env = Object.assign(process.env, loadEnv(mode, "../", ""));
 
   return defineConfig({
