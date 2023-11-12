4fe6b42185bace2ff48dc7256464084ccac010ce
Tyler O'Neil <tyleroneil72@gmail.com>
2023-11-12 14:41:13 -0500

    Add comments to ChatInput.jsx

--- frontend/src/components/ChatInput.jsx
+++ frontend/src/components/ChatInput.jsx
@@ -6,10 +6,10 @@ const ChatInput = ({ value, onChange, onKeyDown, placeholder, onClick }) => {
     <div className='chat-footer'>
       <input
         type='text'
-        value={value}
-        onChange={onChange}
-        onKeyDown={onKeyDown}
-        placeholder={placeholder}
+        value={value} // The value of the input field
+        onChange={onChange} // Function to handle input change in input field
+        onKeyDown={onKeyDown} // Function to handle key down in input field
+        placeholder={placeholder} // Placeholder text for input field
       />
       <button id='send-button' onClick={onClick}>
         Send
