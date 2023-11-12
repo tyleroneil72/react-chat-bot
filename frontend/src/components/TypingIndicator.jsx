18fcd0352921c436bd17990f5c7d79a9c477e754
Tyler O'Neil <tyleroneil72@gmail.com>
2023-11-12 14:44:41 -0500

    Add comment to TypingIndicator.jsx

--- frontend/src/components/TypingIndicator.jsx
+++ frontend/src/components/TypingIndicator.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../css/TypingIndicator.css";
 
 const TypingIndicator = ({ typingIndicatorMessage }) => {
+  // Renders the typing indicator with the correct message
   return <div className='typing-indicator'>{typingIndicatorMessage}</div>;
 };
 
