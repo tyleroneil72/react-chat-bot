5b2cce17ef7d3293dcd838ae440a743b4dcaf9d3
Tyler O'Neil <tyleroneil72@gmail.com>
2023-11-12 14:35:07 -0500

    Add comments to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import Chat from "./chat/Chat.jsx";
 import Footer from "./components/Footer.jsx";
 import "./css/App.css";
-
+// The main App component
 const App = () => {
   return (
+    // Using React fragments to avoid unnecessary divs
     <>
       <main>
         <Chat />
