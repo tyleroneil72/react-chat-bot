11e5c197b19864faf274e5d8fdfc85271324afde
Tyler O'Neil <tyleroneil72@gmail.com>
2023-11-12 14:36:32 -0500

    Add comments to Main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  // StrictMode is a tool for highlighting potential problems in an application during development.
   <React.StrictMode>
     <App />
   </React.StrictMode>
