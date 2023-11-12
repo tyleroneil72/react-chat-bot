ad713b3ca3c9a65a96f9d2e543cd9d23fef4c11e
Tyler O'Neil <tyleroneil72@gmail.com>
2023-11-12 14:53:10 -0500

    Add comment to index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,6 +3,7 @@ const { handleMessage } = require("../controllers/messageController");
 
 const router = express.Router();
 
+// Define a POST route for "/message" that uses the handleMessage function when accessed
 router.post("/message", handleMessage);
 
 module.exports = router;
