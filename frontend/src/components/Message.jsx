32cc9dc50d91769ff1365124a32d8c5f4fc82650
Tyler O'Neil <tyleroneil72@gmail.com>
2023-11-12 14:43:22 -0500

    Add comment to Message.jsx

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -3,6 +3,7 @@ import "../css/Message.css";
 
 const Message = ({ message, type }) => {
   return (
+    // Class is set to either user-message or chatbot-message depending on the type prop
     <div className={type === "user" ? "user-message" : "chatbot-message"}>
       {message}
     </div>
